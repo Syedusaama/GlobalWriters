e047e251b16873e82fbb1a9f56382fac3d8adfef
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-04 19:08:47 +0500

    update

--- index.php
+++ index.php
@@ -1,33 +1,9 @@
-
 <!DOCTYPE html>
 <html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
     <title>Ghost Writers for Hire | Hire Best Ghostwriters for Book & eBooks</title>
-    <meta name="description"
-          content="Ghostwriters for hire services at The Universal Writers! The ghostwriting team initiates and brings you the best form of your ideas in writing! Hire our professional ghost writers for your business book, eBook, Autobiography or memoir.">
-        <meta name="ip2loc" content="">
-<meta name="ip2loc" content="101.53.226.166">
-<meta name="page_url" content="theuniversalwriters.com/">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-<link rel="icon" href="https://theuniversalwriters.com/assets/images/favicon.png" type="image/gif" sizes="16x16">
-    <link rel="stylesheet" href="/assets/css/layout.css" />
-<link rel="stylesheet" href="/assets/css/main.css" />
-<link rel="stylesheet" href="/assets/css/responsive.css" />
-<link rel="stylesheet" href="/assets/css/jquery.fancybox.min.css" />
-<link rel="canonical" href="https://theuniversalwriters.com/">
-
-<!-- Begin Inspectlet Asynchronous Code -->
-<script type="text/javascript">
-(function() {
-window.__insp = window.__insp || [];
-__insp.push(['wid', 1262128767]);
-var ldinsp = function(){
-if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=1262128767&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
-setTimeout(ldinsp, 0);
-})();
-</script>
-<!-- End Inspectlet Asynchronous Code -->
+    <?php include "./components/head.php"?>
 </head>
 <body>
 <div class="wrapper">
