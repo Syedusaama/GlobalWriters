07be213d20609dd3917e804edf97fe1b2d27e2c4
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-04 20:02:47 +0500

    update

--- components/header.php
+++ components/header.php
@@ -5,16 +5,16 @@
                 <div class="col-md-2 logoMainWrap">
                     <div class="logoMain">
                         <a href="https://theuniversalwriters.com/">
-                            <img src="/assets/images/logo.webp" alt="Logo"/>
+                            <img src="./assets/images/logo.webp" alt="Logo"/>
                         </a>
                         <div class="mobileMenuIcon">
-                            <img src="/assets/images/menuIcon.svg" alt="MenuIcon"/>
+                            <img src="./assets/images/menuIcon.svg" alt="MenuIcon"/>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-7 headerMenuMain">
                     <div class="closeMenu mobileMenuIcon">
-                        <img src="/assets/images/closeMenu.svg" alt="Close Menu"/>
+                        <img src="./assets/images/closeMenu.svg" alt="Close Menu"/>
                     </div>
                     <div class="headerMenuWrap">
                         <ul>
@@ -22,13 +22,13 @@
                             <li class="hasChild parentMenu">
                     <span><a href="javascript:;">Ghostwriting <i class="fa-solid fa-chevron-down"></i></a>
                             <ul class="dropDown">
-                              <li><a href="/ghostwriting-services.html">Ghostwriting</a></li>
-                              <li><a href="/ebook-writing.html">Ebook Writers</a></li>
-                              <li><a href="/memoir-writing.html">Memoir Writing</a></li>
-                              <li><a href="/biography-writing.html">Biography Writing</a></li>
-                              <li><a href="/business-book-writing.html">Business Book Writing</a></li>
-                              <li><a href="/non-fiction-writing.html">Non-Fiction</a></li>
-                              <li><a href="/children-book-writing.html">Children's Book Writing</a></li>
+                              <li><a href="/ghostwriting-services.php">Ghostwriting</a></li>
+                              <li><a href="/ebook-writing.php">Ebook Writers</a></li>
+                              <li><a href="/memoir-writing.php">Memoir Writing</a></li>
+                              <li><a href="/biography-writing.php">Biography Writing</a></li>
+                              <li><a href="/business-book-writing.php">Business Book Writing</a></li>
+                              <li><a href="/non-fiction-writing.php">Non-Fiction</a></li>
+                              <li><a href="/children-book-writing.php">Children's Book Writing</a></li>
                             </ul>
                         <div class="openDropdown"><i class="fa-solid fa-chevron-down"></i></div>
                      </span>
@@ -36,16 +36,16 @@
                             <li class="hasChild parentMenu"><span><a href="javascript:;">Fiction Writing <i
                                                 class="fa-solid fa-chevron-down"></i></a>
                         <ul class="dropDown">
-                          <li><a href="/fiction-writing.html">Fiction Writing</a></li>
-                          <li><a href="/action-writing.html">Action Adventure</a></li>
-                          <li><a href="/fantasy-writing.html">Fantasy Writing</a></li>
-                          <li><a href="/science-fiction-writing.html">Sci-Fi Writing</a></li>
-                          <li><a href="/horror-book-writing.html">Horror Writing</a></li>
-                          <li><a href="/drama-writing.html">Drama Writing</a></li>
-                          <li><a href="/adventure-writing.html">Adventure Writing</a></li>
-                          <li><a href="/suspense-thriller-writing.html">Suspense/Thriller Writing</a></li>
-                          <li><a href="/romance-writing.html">Romance Writing</a></li>
-                          <li><a href="/mystery-writing.html">Mystery Writing</a></li>
+                          <li><a href="/fiction-writing.php">Fiction Writing</a></li>
+                          <li><a href="/action-writing.php">Action Adventure</a></li>
+                          <li><a href="/fantasy-writing.php">Fantasy Writing</a></li>
+                          <li><a href="/science-fiction-writing.php">Sci-Fi Writing</a></li>
+                          <li><a href="/horror-book-writing.php">Horror Writing</a></li>
+                          <li><a href="/drama-writing.php">Drama Writing</a></li>
+                          <li><a href="/adventure-writing.php">Adventure Writing</a></li>
+                          <li><a href="/suspense-thriller-writing.php">Suspense/Thriller Writing</a></li>
+                          <li><a href="/romance-writing.php">Romance Writing</a></li>
+                          <li><a href="/mystery-writing.php">Mystery Writing</a></li>
                         </ul>
                         <div class="openDropdown"><i class="fa-solid fa-chevron-down"></i></div>
                      </span>
@@ -53,8 +53,8 @@
                             <li class="hasChild parentMenu"><span><a href="javascript:;">Editing & Formatting <i
                                                 class="fa-solid fa-chevron-down"></i></a>
                         <ul class="dropDown">
-                            <li><a href="/book-editing-services.html">Book Editing</a></li>
-                          <li><a href="/book-formatting-services.html">Book Formatting</a></li>
+                            <li><a href="/book-editing-services.php">Book Editing</a></li>
+                          <li><a href="/book-formatting-services.php">Book Formatting</a></li>
                         </ul>
                         <div class="openDropdown"><i class="fa-solid fa-chevron-down"></i></div>
                      </span>
@@ -62,16 +62,16 @@
                             <li class="hasChild parentMenu"><span><a href="javascript:;">Other Services <i
                                                 class="fa-solid fa-chevron-down"></i></a>
                         <ul class="dropDown">
-                            <li><a href="/book-marketing.html">Book Marketing</a></li>
-                          <li><a href="/book-publishing.html">Book Publishing</a></li>
-                          <li><a href="/audiobook-narrators.html">Audio Books</a></li>
-                          <li><a href="/author-website-design.html">Author Web Design</a></li>
+                            <li><a href="/book-marketing.php">Book Marketing</a></li>
+                          <li><a href="/book-publishing.php">Book Publishing</a></li>
+                          <li><a href="/audiobook-narrators.php">Audio Books</a></li>
+                          <li><a href="/author-website-design.php">Author Web Design</a></li>
                         </ul>
                         <div class="openDropdown"><i class="fa-solid fa-chevron-down"></i></div>
                      </span>
                             </li>
-                             <li><a href="/blogs.html">BLOG</a></li>
-                            <li><a href="/contact-us.html">CONTACT</a></li>
+                             <li><a href="/blogs.php">BLOG</a></li>
+                            <li><a href="/contact-us.php">CONTACT</a></li>
                         </ul>
                     </div>
                 </div>
--- index..php
+++ index..php
@@ -42,59 +42,59 @@
                     <div class="circle">
                         <ul>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/worldwide-racism-pandemic-project.webp"
+                                <img src="./assets/images/portfolio/worldwide-racism-pandemic-project.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/a-boys-dream.webp"
+                                <img src="./assets/images/portfolio/a-boys-dream.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/book-portfolio.webp"
+                                <img src="./assets/images/portfolio/book-portfolio.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/delivered.webp"
+                                <img src="./assets/images/portfolio/delivered.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/died-ten-times-yet-lived.webp"
+                                <img src="./assets/images/portfolio/died-ten-times-yet-lived.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/fae-born-Inure.webp"
+                                <img src="./assets/images/portfolio/fae-born-Inure.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/nikitas-federation.webp"
+                                <img src="./assets/images/portfolio/nikitas-federation.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/the-absolutely.webp"
+                                <img src="./assets/images/portfolio/the-absolutely.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/the-artfully-lived-life.webp"
+                                <img src="./assets/images/portfolio/the-artfully-lived-life.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/the-asylum-frineds.webp"
+                                <img src="./assets/images/portfolio/the-asylum-frineds.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/the-triumph-of-the-human-spirit.webp"
+                                <img src="./assets/images/portfolio/the-triumph-of-the-human-spirit.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/tromp-tapa-blanda.webp"
+                                <img src="./assets/images/portfolio/tromp-tapa-blanda.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/un-trentino-in-america.webp"
+                                <img src="./assets/images/portfolio/un-trentino-in-america.webp"
                                       alt="Book Image"/>
                             </li>
                             <li>
-                                <img src="https://theuniversalwriters.com/assets/images/portfolio/worldwide-domestic-violence-pandemic-project.webp"
+                                <img src="./assets/images/portfolio/worldwide-domestic-violence-pandemic-project.webp"
                                       alt="Book Image"/>
                             </li>
                         </ul>
@@ -119,7 +119,7 @@
                         Hire Top-Notch Book Ghostwriters to Write Inspiring Tales!
                     </h2>
                     <div class="cupImage">
-                        <img src="https://theuniversalwriters.com/assets/images/cup.webp"  alt="Cup Image"/>
+                        <img src="./assets/images/cup.webp"  alt="Cup Image"/>
                     </div>
                 </div>
                 <div class="col-md-6 abtRight">
@@ -1054,158 +1054,9 @@
     </div>
 </section>
 <!-- Footer -->
-    <footer>
-    <div class='container-fluid'>
-        <div class='row'>
-            <div class='col-md-4'>
-                <div class='footerLeft'>
-                <span class='footerTitle'>Crafting Stories, 
-                  Now Just a Click 
-                    Away </span>
-                    <ul class='footerContactDetails'>
-                        <li><a href="mailto:hello@theuniversalwriters.com">hello@theuniversalwriters.com</a></li>
-                        <li><a href="tel:2393009418">(239) 300-9418</a></li>
-                    </ul>
-                    <ul class='socialFooter'>
-                        <li><a href="https://www.facebook.com/TheUniversalWriters#" target="_blank"><span>Facebook</span></a></li>
-                        <li><a href="https://www.instagram.com/theuniversalwriters1/" target="_blank"><span>Instagram</span></a></li>
-                        <li><a href="https://twitter.com/TheUniversalWr" target="_blank"><span>Twitter</span></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class='col-md-8'>
-                <div class='footerRightTop'>
-                    <div class='footerRightWrap'>
-                        <ul class='FooterMenu padingRight'>
-                            <li><a href="/author-website-design/">Author Website Design</a></li>
-                            <li><a href="/ebook-writing/">E-Book Writers</a></li>
-                            <li><a href="/business-book-writing/">Business Book</a></li>
-                            <li><a href="/audiobook-narrators/">Audiobook</a></li>
-                            <li><a href="/blogs/">Blog</a></li>
-                        </ul>
-                    </div>
-                    <div class='footerRightWrap'>
-                        <span class='FooterMenuTitle'><a href="https://theuniversalwriters.com/ghostwriting-services">Ghostwriting</a></span>
-                        <ul class='FooterMenu lightMenu'>
-                            <li><a href="/fiction-writing/">Fiction</a></li>
-                            <li><a href="/non-fiction-writing/">Non-Fiction</a></li>
-                            <li><a href="/biography-writing/">Biography Writing</a></li>
-                            <li><a href="/memoir-writing/">Memoir Writing</a></li>
-                        </ul>
-                    </div>
-                    <div class='footerRightWrap'>
-                        <span class='FooterMenuTitle'><a href="/book-editing-services/">Book Editing</a></span>
-                        <ul class='FooterMenu lightMenu'>
-                            <li><a href="/book-formatting-services/">Formatting</a></li>
-                        </ul>
-                        <ul class='FooterMenu padingRight'>
-                            <li><a href="/book-publishing/">Book Publishing</a></li>
-                            <li><a href="/book-marketing/">Book Marketing</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class='copyRighgtBottom'>
-                    <ul>
-                        <li><a href="/terms-refund/">Terms & Refund</a></li>
-                        <li><a href="/privacy-policy/">Privacy Policy</a></li>
-                        <li><a href="/disclaimer/">Disclaimer</a></li>
-                        <li><a href="/terms-condition/">Terms & Conditions</a></li>
-                    </ul>
-                    <div class='copyRightContent'>
-                        <p>
-                            <i class="fa-solid fa-location-pin"></i>
-                            Woodland Corporate Center, 8270 Woodland Center Blvd, Tampa, FL 33614, United States
-                        </p>
-                    </div>
-                    <div class='copyRightContent'>
-                        <p>© 2024 Global Writers. All Rights Reserved</p>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php include "./components/footer.php"?>
 
 <!-- Footer -->
-    <div class="StickyCta">
-    <div class="StickeyCtaInner">
-        <ul>
-            <li><a href="javascript:;" class="openPopup"><img src="/assets/images/ctaFormfixed.png" alt="Sticky Cta"/></a></li>
-            <li><a href="tel:2393009418"><img src="/assets/images/ctaCallfixed.png" alt="Sticky Cta"/></a></li>
-            <!--<li><a href="javascript:;"><img src="https://theuniversalwriters.com/assets/images/ctaChatfixed.png" alt="Sticky Cta"/></a>-->
-            <!--</li>-->
-        </ul>
-    </div>
-</div>
-<div class='popupMain'>
-    <div class='popuInner'>
-        <div class='popupBox'>
-            <div class='popupClose'>
-                <img src="https://theuniversalwriters.com/assets/images/popupClose.svg" alt="Popup Close"/>
-            </div>
-            <div class='popuptitle'>
-                <span class='popTitMain'>50% Discount!</span>
-                <span class='popTitMainup'>Your Bestselling book is just a click away</span>
-                <p>Schedule a call and talk to our book writing experts</p>
-            </div>
-            <div class='popupForm BannerFields'>
-                <form action="/mail.php" method="post" autocomplete="off">
-                    <div class="formFields">
-                        <img src="https://theuniversalwriters.com/assets/images/nameicon.svg" alt="Icon"/>
-                        <input type="text" placeholder="Enter Your Name" name='Name' required/>
-                    </div>
-                    <div class="formFields">
-                        <img src="https://theuniversalwriters.com/assets/images/emailicon.svg" alt="Icon"/>
-                        <input type="email" placeholder="Enter Your Email" name='Email' required/>
-                    </div>
-                    <div class="formFields">
-                        <img src="https://theuniversalwriters.com/assets/images/phoneicon.svg" alt="Icon"/>
-                        <input type="text" placeholder="Your Phone Number" name='Number' required/>
-                    </div>
-                    <div class="formFields textareapopup">
-                        <textarea placeholder="Type your message here" name='msg'></textarea>
-                    </div>
-                    <div class="formFields FieldButton">
-                        <button type="submit" class="btnPrimary">
-                <span>
-                  Submit <i class="fa-solid fa-arrow-right"></i>
-                </span>
-                        </button>
-                    </div>
-                <div class="hidden-fields">
-                    <!-- Customer Info -->
-                    <input type="hidden" name="fullpageurl" value=""/>
-                    <input type="hidden" name="pageurl" value=""/>
-                    <!-- ip2Location -->
-                    <input type="hidden" name="ip2loc_ip" value=""/>
-                    <input type="hidden" name="ip2loc_isp" value=""/>
-                    <input type="hidden" name="ip2loc_org" value=""/>
-                    <input type="hidden" name="ip2loc_country"
-                           value=""/>
-                    <input type="hidden" name="ip2loc_region"
-                           value=""/>
-                    <input type="hidden" name="ip2loc_city" value=""/>
-                    <input type="hidden" name="pc" value="">
-                    <input type="hidden" name="hiddencapcha" value="">
-                    <input type="hidden" name="setCookie" value="setBannerCookie">
-                    <input type="hidden" name="pack_name" class="has-Pack" value="">
-                    <input type="hidden" name="setURL" class="packname" value="">
-                    <input type="hidden" name="pac" class="packp" value="">
-                    <input type="hidden" id="location" name="locationURL"
-                           value="http://theuniversalwriters.com/"/>
-                    <script type="text/javascript">document.getElementById('location').value = window.location.href;</script>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
 
-<script src="./assets/js/plugin.js"></script>
-<script src="./assets/js/jquery.fancybox.min.js"></script>
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"></script>
-<script src="./assets/js/custom.js?v=25874"></script>
 </body>
 </html>
