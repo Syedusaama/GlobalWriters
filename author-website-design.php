3ec0e96552706986625e40bff8787f867f02f714
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-09 21:12:36 +0500

    update

--- author-website-design.php
+++ author-website-design.php
@@ -213,52 +213,9 @@
             </div>
         </section>
 
-        <section class="sec9 ctaGreen">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 ctaTitleLeft">
-                        <span class="heading2 boldheading2">Design a unique and engaging author’s website for your
-                            ebooks!</span>
-                        <p>Connect with us now to get the best deals!</p>
-                    </div>
-                    <div class="col-md-6 ctaTitleRight">
-                        <div class="ctaTitleRightInnerLeft">
-                            <div class="ctaWrap">
-                                <div class="aboutCta">
-                                    <!--<a href="javascript:;" class="ctaLinePhone">-->
-                                    <!--    <i class="fa-solid fa-comment"></i>-->
-                                    <!--    <span>Chat With US</span>-->
-                                    <!--</a>-->
-                                    <a href="tel:2393009418" class="ctaLinePhone">
-                                        <i class="fa-solid fa-phone"></i>
-                                        <span class="smallTitleLine">We will be happy to assist you</span>
-                                        (239) 300-9418
-                                    </a>
-                                    <a href="javascript:;" class="btnPrimary openPopup">
-                                        <span>Request A Quote <i class="fa-solid fa-arrow-right"></i></span>
-                                    </a>
-                                </div>
-                                <div class="ctabtnrightsocial">
-                                    <div class="trustpilotGoogleBox">
-                                        <div class="reviewBTn">
-                                            <a href="https://www.trustpilot.com/review/theuniversalwriters.com"
-                                                target="_blank">
-                                                <img src="./assets/images/trustpilot.webp" alt="Trustpilot Images" />
-                                            </a>
-                                        </div>
-                                        <div class="reviewBTn">
-                                            <a href="https://goo.gl/maps/SLykFoQzEiTwNbHd9" target="_blank">
-                                                <img src="./assets/images/google.webp" alt="Google Images" />
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <!-- Cta-Center -->
+        <?php include "./components/cta-center.php" ?>
+        <!-- Cta-Center -->
 
         <section class="sec12 ghostagecnyContent">
             <div class="container">
@@ -483,9 +440,10 @@
             </div>
         </section>
 
-<!-- CTA -->
-<?php include "./components/cta-footer.php" ?>
-<!-- CTA -->
+        <!-- CTA -->
+        <?php include "./components/cta-footer.php" ?>
+        <!-- CTA -->
+
         <!-- footer -->
         <?php include "./components/footer.php" ?>
 
