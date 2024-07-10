56becc84bd04915175332a7c602e0022dd0802a4
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-10 21:31:02 +0500

    update

--- components/cta-center.php
+++ components/cta-center.php
@@ -1,4 +1,4 @@
-<section class="sec9 ctaGreen">
+<section class="sec9 ctaGreen whiteCta">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 ctaTitleLeft">
--- components/cta-footer.php
+++ components/cta-footer.php
@@ -1,4 +1,4 @@
-<section class="sec9 ctaOrange">
+<section class="sec9 ctaOrange whiteCta">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 ctaTitleLeft">
