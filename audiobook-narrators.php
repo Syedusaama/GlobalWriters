1ffb1402cb5d819383e42cf52ad0bdf0f0bf0e85
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-11 20:33:26 +0500

    update

--- audiobook-narrators.php
+++ audiobook-narrators.php
@@ -250,8 +250,8 @@
                                             src="./assets/images/animations/audiobook-narrators/Industry-Leading-Audiobook-Experts.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/veoexymv.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#471758"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
@@ -276,8 +276,8 @@
                                             src="./assets/images/animations/audiobook-narrators/PremiumQualityAffordableAudiobook.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/lsrcesku.json" trigger="loop"
+                                            delay="2000"  colors="primary:#000000,secondary:#471758,tertiary:#d8bdf7,quaternary:#d8bdf7"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
@@ -305,8 +305,8 @@
                                             src="./assets/images/animations/audiobook-narrators/Tech-SavvyDownloadableDigitalFormats.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/kiynvdns.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#d8bdf7,quaternary:#c69cf4,quinary:#c69cf4"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
@@ -333,8 +333,8 @@
                                             src="./assets/images/animations/audiobook-narrators/ProfessionalNarrationandStory-TellingCompany.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/ujxzdfjx.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
