1fefef17f30cd53fff61e83e6acc30782e30f5df
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-11 20:41:38 +0500

    update

--- mystery-writing.php
+++ mystery-writing.php
@@ -202,10 +202,10 @@
                                             src="./assets/images/animations/mystery-writing/ExcitingandUniquePlotlines.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/veoexymv.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#471758"
                                             style="width:100px;height:100px">
-                                        </lord-icon> 
+                                        </lord-icon>
                                 </div>
                                 <div class="contentChoose">
                                     <span>Exciting and Unique Plotlines </span>
@@ -224,10 +224,10 @@
                                             src="./assets/images/animations/mystery-writing/OngoingSupport.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/lsrcesku.json" trigger="loop"
+                                            delay="2000"  colors="primary:#000000,secondary:#471758,tertiary:#d8bdf7,quaternary:#d8bdf7"
                                             style="width:100px;height:100px">
-                                        </lord-icon> 
+                                        </lord-icon>
                                 </div>
                                 <div class="contentChoose">
                                     <span>Ongoing Support </span>
@@ -248,10 +248,10 @@
                                             src="./assets/images/animations/mystery-writing/ChoosetheMostAffordableServices.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/kiynvdns.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#d8bdf7,quaternary:#c69cf4,quinary:#c69cf4"
                                             style="width:100px;height:100px">
-                                        </lord-icon>   
+                                        </lord-icon>  
                                 </div>
                                 <div class="contentChoose">
                                     <span>Choose the Most Affordable Services </span>
@@ -272,8 +272,8 @@
                                             src="./assets/images/animations/mystery-writing/UnravelMultipleTonesandAtmospheres.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/ujxzdfjx.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7"
                                             style="width:100px;height:100px">
                                         </lord-icon>  
                                 </div>
--- romance-writing.php
+++ romance-writing.php
@@ -207,10 +207,10 @@
                                                 src="./assets/images/animations/romance-writing/NoGenreIsImpossibleForTheUniversalWriters.json"
                                                 background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/veoexymv.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#471758"
                                             style="width:100px;height:100px">
-                                        </lord-icon>                                 
+                                        </lord-icon>                                
                                             </div>
                                 <div class="contentChoose">
                                     <span>No Genre Is Impossible For The Universal Writers </span>
@@ -229,10 +229,10 @@
                                             src="./assets/images/animations/romance-writing/CreativeBrainstormingSessions.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/lsrcesku.json" trigger="loop"
+                                            delay="2000"  colors="primary:#000000,secondary:#471758,tertiary:#d8bdf7,quaternary:#d8bdf7"
                                             style="width:100px;height:100px">
-                                        </lord-icon>                                 
+                                        </lord-icon>                                
                                         </div>
                                 <div class="contentChoose">
                                     <span>Creative Brainstorming Sessions</span>
@@ -255,10 +255,10 @@
                                             src="./assets/images/animations/romance-writing/TimelyDeliveries.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/kiynvdns.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#d8bdf7,quaternary:#c69cf4,quinary:#c69cf4"
                                             style="width:100px;height:100px">
-                                        </lord-icon>                                 
+                                        </lord-icon>                                
                                         </div>
                                 <div class="contentChoose">
                                     <span>Timely Deliveries </span>
@@ -279,8 +279,8 @@
                                             src="./assets/images/animations/romance-writing/FreeConsultation.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/ujxzdfjx.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7"
                                             style="width:100px;height:100px">
                                         </lord-icon>                                 
                                         </div>
