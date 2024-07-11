9181375139afd88dae11eb83e39a0ad6e5737b61
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-11 21:20:01 +0500

    update

--- action-writing.php
+++ action-writing.php
@@ -227,8 +227,8 @@
                                             src="./assets/images/animations/action-writing/CraftingStoriesNotJustWords.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/veoexymv.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#471758"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
@@ -249,8 +249,8 @@
                                             src="./assets/images/animations/action-writing/BeYourOwnBoss.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/lsrcesku.json" trigger="loop"
+                                            delay="2000"  colors="primary:#000000,secondary:#471758,tertiary:#d8bdf7,quaternary:#d8bdf7"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
@@ -273,8 +273,8 @@
                                             src="./assets/images/animations/action-writing/UnleashYourImagination.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/kiynvdns.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#d8bdf7,quaternary:#c69cf4,quinary:#c69cf4"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
@@ -298,8 +298,8 @@
                                             src="./assets/images/animations/action-writing/MultipleFeatures.json"
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-                                        <lord-icon src="https://cdn.lordicon.com/jtiihjyw.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#e5d1fa,tertiary:#7e489c"
+                                        <lord-icon src="https://cdn.lordicon.com/ujxzdfjx.json" trigger="loop"
+                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7"
                                             style="width:100px;height:100px">
                                         </lord-icon>
                                     </div>
