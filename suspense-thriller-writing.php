7ead1f9823a4a464869e265d74b061ff9d99863d
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-07-22 20:45:17 +0500

    update

--- suspense-thriller-writing.php
+++ suspense-thriller-writing.php
@@ -206,7 +206,7 @@
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
                                         <lord-icon src="https://cdn.lordicon.com/veoexymv.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#471758"
+                                            delay="2000" colors="primary:#005d56,secondary:#10857e,tertiary:#20b2aa"
                                             style="width:100px;height:100px">
                                         </lord-icon>                                
                                         </div>
@@ -228,7 +228,7 @@
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
                                         <lord-icon src="https://cdn.lordicon.com/lsrcesku.json" trigger="loop"
-                                            delay="2000"  colors="primary:#000000,secondary:#471758,tertiary:#d8bdf7,quaternary:#d8bdf7"
+                                            delay="2000"  colors="primary:#005d56,secondary:#20b2aa,tertiary:#ffffff,quaternary:#10857e"
                                             style="width:100px;height:100px">
                                         </lord-icon>                                
                                         </div>
@@ -255,7 +255,7 @@
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
                                         <lord-icon src="https://cdn.lordicon.com/kiynvdns.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7,tertiary:#d8bdf7,quaternary:#c69cf4,quinary:#c69cf4"
+                                            delay="2000" colors="primary:#005d56,secondary:#10857e,tertiary:#10857e,quaternary:#20b2aa,quinary:#20b2aa"
                                             style="width:100px;height:100px">
                                         </lord-icon>                                
                                         </div>
@@ -278,7 +278,7 @@
                                             background="transparent" speed="1" loop="" autoplay=""></lottie-player> -->
                                             <script src="https://cdn.lordicon.com/lordicon.js"></script>
                                         <lord-icon src="https://cdn.lordicon.com/ujxzdfjx.json" trigger="loop"
-                                            delay="2000" colors="primary:#121331,secondary:#d8bdf7"
+                                            delay="2000" colors="primary:#005d56,secondary:#10857e"
                                             style="width:100px;height:100px">
                                         </lord-icon>                                 
                                         </div>
